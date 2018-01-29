354362630618602f4b08becc4ed88019dad62b3e
ProfessorGenki <getyowalkon@gmail.com>
2018-01-29 10:49:29 +0100

    Add project files.

---
+++ EmptyClassRoomRepo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmptyClassRoomRepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
